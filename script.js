217df0bdf329092a7dbbc08cc70d9d45cff05cf3
unknown <noepouth02@gmail.com>
2024-09-10 19:11:18 +0100

    first commit

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ async function recupPokemonsDetail(name) {
     const dataPokemon = await reponsePokemon.json()
     return dataPokemon
 }
+
+// fonction pour les types de pokemons
+function elementsColor(type) {
+    const typeColors = {
+        fire: '#f08030',
+        water: '#6890f0',
+        grass: '#78c850',
+        electric: '#f8d030',
+        ice: '#98d8d8',
+        fighting: '#c03028',
+        poison: '#a040a0',
+        ground: '#e0c068',
+        flying: '#a890f0',
+        psychic: '#f85888',
+        bug: '#a8b820',
+        rock: '#b8a038',
+        ghost: '#705898',
+        dragon: '#7038f8',
+        dark: '#705848',
+        steel: '#b8b8d0',
+        fairy: '#f0b6bc',
+        normal: '#a8a878'
+}
+return typeColors[type] || '#a8a878'
+}
